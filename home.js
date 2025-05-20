5d2cbfe88e62c3f077ef1ec8568f9278bc8d3a6d
Bruna Oliveira Vasconcelos <brunavasconcelos@seai.am.gov.br>
2025-05-20 17:17:11 -0400

    home

--- home.js
+++ home.js
@@ -36,3 +36,54 @@ document.querySelectorAll('.carousel-wrapper').forEach(wrapper => {
     carousel.scrollBy({ left: 300, behavior: 'smooth' });
   });
 });
+
+const obras = [
+  {
+    nome: "Jujutsu Kaisen",
+    imagem: "assets-melhores-avaliados/Jujutsu Kaisen  2020 ‧ Büyü ‧ 2 sezon 1.png",
+    nota: 5
+  },
+  {
+    nome: "Game of Thrones",
+    imagem: "assets-melhores-avaliados/Game of Thrones (2011).png",
+    nota: 5
+  },
+  {
+    nome: "Oppenheimer",
+    imagem: "assets-melhores-avaliados/Oppenheimer.png",
+    nota: 4
+  },
+  {
+    nome: "O Menino e a Garça",
+    imagem: "assets-melhores-avaliados/The Boy And The Heron(Studio Ghibli,2023).png",
+    nota: 4
+  },
+  {
+    nome: "Mickey 17",
+    imagem: "assets-melhores-avaliados/baixados.png",
+    nota: 3
+  },
+  {
+    nome: "Stranger Things",
+    imagem: "assets-melhores-avaliados/baixados (9).png",
+    nota: 4
+  }
+];
+
+const container = document.getElementById('ranking-list');
+
+obras.forEach((obra, index) => {
+  const item = document.createElement('div');
+  item.classList.add('ranking-item');
+
+  item.innerHTML = `
+    <div class="ranking-number">${index + 1}</div>
+    <img src="${obra.imagem}" alt="${obra.nome}" class="ranking-image">
+    <div class="ranking-info">
+      <div class="ranking-title">${obra.nome}</div>
+      <div class="stars">${'★'.repeat(obra.nota)}${'☆'.repeat(5 - obra.nota)}</div>
+    </div>
+  `;
+
+  container.appendChild(item);
+});
